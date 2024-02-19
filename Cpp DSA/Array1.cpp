8138970990030af4bb5a8d2a727ac354e00696f1
adulbasit <bassitb50@gmail.com>
2024-02-19 23:59:27 +0500

    NotNull

---
+++ Cpp DSA/Array1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	//Create an Array
+	int Array[4] = {11,22,33,55};
+	cout<<Array[0]<<"\n";
+	cout<<Array[3]<<"\n";
+	cout<<Array[1]<<"\n";
+	cout<<Array[2];
+	return 0;
+}
+
+
